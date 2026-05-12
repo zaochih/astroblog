2a19b1b62d2a25d534c8f0f30eb5714b2bb9bed5
Caozhi Li <git@licaoz.com>
2026-05-12 19:47:36 +0800

    feat: add Tailwind CSS configuration for typography customization

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'blockquote p:first-of-type::before': {
+              content: 'none',
+            },
+            'blockquote p:last-of-type::after': {
+              content: 'none',
+            },
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+          },
+        },
+      },
+    },
+  },
+};
